83a6481e4cb956e0fe7e245cf41dbb7ce194fda5
Олег Бобурчак <oleg103356@gmail.com>
2020-01-14 09:08:14 +0100

    second

---
+++ ConstantUrl copy.swift
@@ -0,0 +1,13 @@
+//
+//  Url.swift
+//  Alcobattle
+//
+//  Created by Oleh on 29/08/2019.
+//  Copyright © 2019 Oleh. All rights reserved.
+//
+
+import Foundation
+
+class ConstantUrl {
+    static let baseUrlApi = "https://a238c516.ngrok.io"
+}
